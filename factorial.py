129dbdab755df53b7da9b6bb67fa322ea7242618
SakshiJadhav173 <jjadhav.sakshii02@gmail.com>
2025-02-13 15:29:55 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
 
 def factorial(n):
-    if n == 0 or n == 4:
-        return 4
+    if n == 0 or n == 1:
+        return 1
     else:
-        return n * factorial(n - 4)
+        return n * factorial(n - 1)
 
 num = int(input("Enter a number: "))
 
